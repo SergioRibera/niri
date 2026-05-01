be2e22a9ef144bcb8e197891177fbcb64a42de80
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-05-01 18:18:07 -0400

    feat: move cursor shake animation to custom animations

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -2630,7 +2630,7 @@ impl State {
 
         pointer.frame(self);
 
-        self.niri.cursor_scale_tracker.on_motion(false, pos);
+        self.niri.cursor_scale_tracker.on_motion(pos);
 
         // contents_under() will return no surface when the hot corner should trigger, so
         // pointer.motion() will set the current focus to None.
@@ -2725,7 +2725,7 @@ impl State {
 
         pointer.frame(self);
 
-        self.niri.cursor_scale_tracker.on_motion(true, pos);
+        self.niri.cursor_scale_tracker.on_motion(pos);
 
         // contents_under() will return no surface when the hot corner should trigger, so
         // pointer.motion() will set the current focus to None.
