4664b71e0455e77af2bcbe34096797c5d03658a0
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2026-05-30 11:24:46 +0530

    patch: queue_redraw_all() -> queue_redraw() in on_pointer_motion* and is_animating for ZoomTransition

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -2857,7 +2857,7 @@ impl<W: LayoutElement> Layout<W> {
 
             if self
                 .zoom_state_ref(&mon.output)
-                .is_some_and(OutputZoomState::transitioning)
+                .is_some_and(OutputZoomState::is_animating)
             {
                 return true;
             }
